fa2d998d1b7b26625041ffb25348b9e4be606c07
wynnlah0913 <wynnwongz@gmail.com>
2025-05-02 01:02:59 +0800

    Update hand_signal.sql

--- SQL DATABASE/hand_signal.sql
+++ SQL DATABASE/hand_signal.sql
@@ -1,37 +1,19 @@
 SHOW DATABASES;
 USE hand_project;
 SHOW TABLES;
-DROP TABLE `user_public`;
-
-CREATE TABLE `user_public`(
+CREATE TABLE `users`(
 	`user_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `username` VARCHAR(25) NOT NULL,
-	`gender` ENUM('M','F'),
-    `age` INT
+    `name` VARCHAR(100) NOT NULL,
+	`gender` ENUM('M', 'F'),
+	`age` INT,
+    `email` VARCHAR(255) UNIQUE NOT NULL,
+    `password` VARCHAR(255) NOT NULL
 );
 
-DESCRIBE `user_public`;
-    
-CREATE TABLE `user_private`(
-	`user_id` INT PRIMARY KEY,
-    `name` VARCHAR(70) NOT NULL,
-	`email` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user_public(user_id) ON DELETE CASCADE
-);    
-
-INSERT INTO `user_public` (username, gender, age)
-VALUES ('alpha', 'M', 24);
-
-SELECT user_id FROM user_public WHERE username = 'Alpha';
-
-INSERT INTO `user_private` (user_id, name, email, password)
-VALUES (1, 'Alpha Livingston', 'alpha@gmail.com', 'imd123');
-
-SELECT * FROM `user_private`;
-
-SELECT u.user_id, u.username, u.gender, u.age, p.name, p.email
-FROM user_public u
-JOIN user_private p ON u.user_id = p.user_id;
-
-SELECT * FROM user_private
+CREATE TABLE `courses`(
+	`course_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(50) UNIQUE NOT NULL,
+	`level` ENUM('Beginner', 'Intermediate', 'Expert'),
+	`desc` TEXT,
+    `thumbnail_url` VARCHAR(255)
+);
