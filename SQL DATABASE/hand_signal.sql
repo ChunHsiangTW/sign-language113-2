d911803bf74def9ad82fde9857177bdc07114d47
wynnlah0913 <wynnwongz@gmail.com>
2025-10-07 09:26:36 +0800

    Update hand_signal.sql

--- SQL DATABASE/hand_signal.sql
+++ SQL DATABASE/hand_signal.sql
@@ -1,51 +1,33 @@
 SHOW DATABASES;
-USE hand_project; 
+USE `114-Handlang`;
 SHOW TABLES;
 
 CREATE TABLE `users`(
-	`user_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(100) NOT NULL,
-	`gender` ENUM('M', 'F', 'Other'),
-	`age` INT,
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(50) UNIQUE NOT NULL,
+    `age` INT,
     `email` VARCHAR(255) UNIQUE NOT NULL,
+    `gender` ENUM('M', 'F', 'Other'),
     `password` VARCHAR(255) NOT NULL
 );
-
-CREATE TABLE `courses`(
-	`course_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `course_title` VARCHAR(50) UNIQUE NOT NULL,
-	`level` ENUM('Beginner', 'Intermediate', 'Expert'),
-	`desc` TEXT,
-    `thumbnail_url` VARCHAR(255)
-);
-
-CREATE TABLE `lessons`(
-	`lesson_id` INT AUTO_INCREMENT PRIMARY KEY,
-	`course_id` INT,
-    `lesson_title` VARCHAR(50) UNIQUE NOT NULL,
-	`video_url` VARCHAR(255),
-	`content` TEXT,
-    FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE
-);
-
-CREATE TABLE `gesture_word`(
-	`gesture_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `word` VARCHAR(50) NOT NULL,
-    `meaning` TEXT,
-    `example` TEXT,
-    `category` VARCHAR(50)
+    
+CREATE TABLE `category`(
+	`category_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `category_title` VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE `gesture_video`(
 	`video_id` INT PRIMARY KEY AUTO_INCREMENT,
-    `gesture_id` INT NOT NULL,
+    `category_id` INT NOT NULL,
+    `word` VARCHAR(50) NOT NULL,
     `video_url` VARCHAR(255),
-    FOREIGN KEY (gesture_id) REFERENCES gesture_word(gesture_id)
+    FOREIGN KEY (category_id) REFERENCES category(category_id)
 );
 
-
-
-
-
-    
-    
+CREATE TABLE `recognize_record`(
+	`record_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `id` INT NOT NULL,
+    `recognized_text` VARCHAR(500) NOT NULL,
+    `recognized_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id) REFERENCES users(id)
+);
